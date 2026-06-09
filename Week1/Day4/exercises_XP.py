d644e5effcaa145c6937bd5fb44bb7b1e04af542
elem86 <elemke86@gmail.com>
2026-06-09 12:11:54 +0300

    Day4, create exercises_XP.py

---
+++ Week1/Day4/exercises_XP.py
@@ -0,0 +1,29 @@
+# 🌟 Exercise 1: Favorite Numbers
+# Key Python Topics:
+
+# Sets
+# Adding/removing items in a set
+# Set concatenation (using union)
+
+
+# Instructions:
+
+# Create a set called my_fav_numbers and populate it with your favorite numbers.
+# Add two new numbers to the set.
+# Remove the last number you added to the set.
+# Create another set called friend_fav_numbers and populate it with your friend’s favorite numbers.
+# Concatenate my_fav_numbers and friend_fav_numbers to create a new set called our_fav_numbers.
+# Note: Sets are unordered collections, so ensure no duplicate numbers are added.
+
+my_fav_numbers = {68, 86, 8, 10, 6}
+
+my_fav_numbers.add(3)
+my_fav_numbers.add(2)
+
+my_fav_numbers.remove(2)
+
+friend_fav_numbers = {1, 2, 3, 4, 8}
+
+our_fav_numbers = my_fav_numbers.union(friend_fav_numbers)
+
+print(our_fav_numbers)
